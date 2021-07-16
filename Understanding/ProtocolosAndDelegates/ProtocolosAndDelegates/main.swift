7d7fdb6e434211aa8a069c493f757ba8eb6f93d1
Gabriel Lucas <gabriellcontatoprofissional@gmail.com>
2021-07-15 21:44:54 -0300

    Adding project and protocols and delegate study files

---
+++ Understanding/ProtocolosAndDelegates/ProtocolosAndDelegates/main.swift
@@ -0,0 +1,61 @@
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happend?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
+
+// This strucuture is able to know how to perform CPR
+struct Paramedic: AdvancedLifeSupport {
+    
+    // We can pretend this code says: I am going to listen for notification when I have to perform CPR from the emergy call handler. I know who the handler is. And when they tell me to perform CPR, that's exactly what I'm gonna do.
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The paramedic does chest compressions, 30 per second.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The doctor does chest compressions, 30 per second.")
+    }
+    
+    func useStethescope() {
+        print("Listening for heart sounds")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Sings stayings alive by the BeeGees")
+    }
+    
+    func useElectrilDrill() {
+        print("Whirrr...")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+//let pete = Paramedic(handler: emilio)
+let angela = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
