ce7ebe8d1539839803c5121c16808fa48a5d9272
Gabriel Lucas <gabriellcontatoprofissional@gmail.com>
2021-07-27 20:03:58 -0300

    Adding challenge project

---
+++ Challenge/ByteCoin/Model/CoinDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoinDataModel.swift
+//  ByteCoin
+//
+//  Created by Gabriel Lucas Alves da Silva on 27/07/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinDataModel: Codable {
+    let rate: Double
+}
---
+++ Challenge/ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,64 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol CoinManagerDelegate {
+    func didUpdateCoin(_ coinManager: CoinManager, _ coin: CoinDataModel)
+    func didFailedWithError(_ error: Error)
+}
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = ""
+    
+    var delegate: CoinManagerDelegate?
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    func getCoinPrice(for currency: String) {
+        let url = baseURL + "/\(currency)/?apikey=\(apiKey)"
+        performRequest(with: url)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    self.delegate?.didFailedWithError(error!)
+                    return
+                }
+                if let safeData = data {
+                    if let coin = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateCoin(self, coin)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+        
+    }
+    
+    func parseJSON(_ coinData: Data) -> CoinDataModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinDataModel.self, from: coinData)
+            let lastPrice = CoinDataModel(rate: decodedData.rate)
+            
+            return lastPrice
+            
+        } catch {
+            self.delegate?.didFailedWithError(error)
+            return nil
+        }
+    }
+    
+}
